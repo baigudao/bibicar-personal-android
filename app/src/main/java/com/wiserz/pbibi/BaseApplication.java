18f2ad8b8e5224b614fafe51f2d2135eec1f3ace
Jackie <971060378@qq.com>
2017-08-09 09:58:10 +0800

    Initial commit

---
+++ app/src/main/java/com/wiserz/pbibi/BaseApplication.java
@@ -0,0 +1,11 @@
+package com.wiserz.pbibi;
+
+import android.app.Application;
+
+/**
+ * Created by jackie on 2017/8/9 9:55.
+ * QQ : 971060378
+ * Used as : 应用的基类
+ */
+public class BaseApplication extends Application{
+}
---
+++ app/src/main/java/com/wiserz/pbibi/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wiserz.pbibi.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wiserz.pbibi.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
