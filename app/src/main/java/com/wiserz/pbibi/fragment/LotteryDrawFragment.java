466b4d0ed8d0dbc141b644505fa8aa6ccf93a7ee
Jackie <971060378@qq.com>
2017-09-26 13:14:43 +0800

    fix

--- app/src/main/java/com/wiserz/pbibi/fragment/LotteryDrawFragment.java
+++ app/src/main/java/com/wiserz/pbibi/fragment/LotteryDrawFragment.java
@@ -59,16 +59,22 @@ public class LotteryDrawFragment extends BaseFragment {
             WebSettings webSettings = tencent_web_view.getSettings();
 
             webSettings.setJavaScriptEnabled(true);
-            webSettings.setSupportZoom(true);
+//            webSettings.setSupportZoom(true);
+
+            // 设置不可缩放
+            webSettings.setSupportZoom(false);
+
             webSettings.setUseWideViewPort(true);
+            webSettings.setLoadWithOverviewMode(true);
+            webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+
+
             webSettings.setDomStorageEnabled(true);
             webSettings.setAppCacheMaxSize(1024 * 1024 * 8);
             String appCachePath = BaseApplication.getAppContext().getCacheDir().getAbsolutePath();
             webSettings.setAppCachePath(appCachePath);
             webSettings.setAllowFileAccess(true);
             webSettings.setAppCacheEnabled(true);
-            webSettings.setLoadWithOverviewMode(true);
-            webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
 
             tencent_web_view.setInitialScale(200);
 
